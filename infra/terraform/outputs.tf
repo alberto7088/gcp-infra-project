f6a5523d4a38b22f18a9f96ab64d1d3115ca78dd
alberto <alberto7088@users.noreply.github.com>
2025-04-22 09:44:44 +0100

    Replaced underscore in function name

--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -1,4 +1,4 @@
 output "word_counter_url" {
   description = "The public URL of the word_counter function"
-  value       = module.word_counter.function_url
+  value       = module.word-counter.function_url
 }
